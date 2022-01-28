908d72cf83d5f969b79db625a1fcf6cddaa13f72
Divyansh Verma <69747865+DivyanshVerma217@users.noreply.github.com>
2022-01-28 15:05:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-// const path = require('path')
+const path = require('path')
 
 
 
@@ -22,14 +22,14 @@ app.use("/api/course/",courseRoute);
 app.use("/api/question/", quesRoute);
 app.use("/api/quiz/", quizRoute);
 
-// app.use(express.static(path.join(__dirname, "client", "build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
-
-app.get("/", (req, res) => {
-  res.send("server working");
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+
+// app.get("/", (req, res) => {
+//   res.send("server working");
+// });
 
 const port = process.env.PORT || 4000;
 
