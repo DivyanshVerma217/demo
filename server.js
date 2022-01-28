a4bbe6481778d13112a1f89cbba39a78290872df
Divyansh verma <123divyanshverma@gmail.com>
2022-01-28 15:03:37 +0530

    final

---
+++ server.js
@@ -0,0 +1,36 @@
+
+// const path = require('path')
+
+
+
+
+const express = require("express");
+const db = require("./db.js");
+
+const app = express();
+
+
+app.use(express.json());
+
+const userRoute = require("./routes/userRoute");
+const courseRoute = require("./routes/courseRoute")
+const quesRoute  = require("./routes/quesRoute")
+const quizRoute = require("./routes/quizRoute")
+
+app.use("/api/users/", userRoute);
+app.use("/api/course/",courseRoute);
+app.use("/api/question/", quesRoute);
+app.use("/api/quiz/", quizRoute);
+
+// app.use(express.static(path.join(__dirname, "client", "build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+
+app.get("/", (req, res) => {
+  res.send("server working");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log("Server is running on port", port));
